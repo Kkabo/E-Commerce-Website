5e5ef0751f1684a6f83522b22f7a2014041f60da
Kkabo -Git <dardankabashi101@gmail.com>
2024-03-20 16:10:32 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Logo from "../assets/Library.svg";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row row__column">
+          <Link to="/">
+            <figure className="foot__logo">
+              <img src={Logo} className="footer__logo--img" alt="" />
+            </figure>
+          </Link>
+          <div className="footer__list">
+            <Link to="/" className="footer__link">
+              Home
+            </Link>
+            <Link to="/books" class="footer__link">
+              Books
+            </Link>
+            <Link to="/cart" class="footer__link">
+              Cart
+            </Link>
+          </div>
+          <div className="footer__copyright">Copyright &copy; 2024 Library</div>
+        </div>
+      </div>
+    </footer>
+  );
+}
